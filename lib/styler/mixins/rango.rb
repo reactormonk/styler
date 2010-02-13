aec1464034005f45df68879f5fc345a9815b2271
Simon Hafner <hafnersimon@gmail.com>
2010-02-13 21:35:21 +0100

    added Styler::RangoMixin

--- lib/styler/mixins/rango.rb
+++ lib/styler/mixins/rango.rb
@@ -7,3 +7,16 @@ module Styler
     end
   end
 end
+
+module Styler
+  module RangoMixin
+    include Rango::ImplicitRendering
+    def render(template)
+      instance_variables.each {|name|
+        instance_variable_set(name,::Styler.new_style_for(instance_variabel_get(name)))
+      }
+      super
+    end
+  end
+end
+
