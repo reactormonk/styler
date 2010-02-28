d0ad59ea9e1fbf0a9bab3fdaee604ea2c47b3575
Simon Hafner <hafnersimon@gmail.com>
2010-02-28 20:01:02 +0100

    make it a bit easier to defined models when calling render

--- lib/styler/mixins/rango.rb
+++ lib/styler/mixins/rango.rb
@@ -14,7 +14,7 @@ module Styler
     def render(template, *models)
       @__controller = self
       models.each {|name|
-        instance_variable_set(name,::Styler.new_style_for(instance_variable_get(name)))
+        instance_variable_set("@#{name}",::Styler.new_style_for(instance_variable_get("@#{name}")))
       }
       super
     end
