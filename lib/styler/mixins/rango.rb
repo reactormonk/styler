e540a1bc062534e26c0487c9c44d4cfd6b9ee51d
Simon Hafner <hafnersimon@gmail.com>
2010-03-15 12:40:05 +0100

    fixed a bug in the rango mixin

--- lib/styler/mixins/rango.rb
+++ lib/styler/mixins/rango.rb
@@ -11,9 +11,14 @@ module Styler
   module RangoMixin
     include Rango::ImplicitRendering
     def render(template, *models)
-      @__controller = self
       models.each {|name|
-        instance_variable_set("@#{name}",::Styler.new_style_for(instance_variable_get("@#{name}")))
+        instance_variable_set(
+          "@#{name}",
+          ::Styler.new_style_for(
+            instance_variable_get("@#{name}"),
+            {__controller: self}
+          )
+        )
       }
       super(template)
     end
